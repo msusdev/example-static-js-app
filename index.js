f84dc647aca97b3ee78391dbbf85c97727b6e31f
Sidney Andrews <v-sandrews@microsoft.com>
2021-08-26 15:06:01 -0400

    Submit example solution

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 async function run() {
     console.log("running...");
+    const config = {
+        auth: {
+            clientId: '<client-id>',
+            authority: 'https://login.microsoftonline.com/organizations/',
+            redirectUri: 'http://localhost:8080'
+        }
+    };
+    var client = new msal.PublicClientApplication(config);
+    
+    var loginRequest = {
+        scopes: [ 'user.read' ]
+    };
+    let loginResponse = await client.loginPopup(loginRequest);
+    console.log('Login Response', loginResponse);
+
+    var tokenRequest = {
+        scopes: [ 'user.read' ],
+        account: loginResponse.account
+    };
+    let tokenResponse = await client.acquireTokenSilent(tokenRequest);
+    console.log('Token Response', tokenResponse);
+
+    let payload = await fetch("https://graph.microsoft.com/v1.0/me", {
+        headers: {
+            'Authorization': 'Bearer ' + tokenResponse.accessToken
+        }
+    });
+    let json = await payload.json();
+    console.log('Graph Response', json);
 }
